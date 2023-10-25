e2ab82e22911631f92e36edfd16003c6ebe46dc5
Mateus Conceição <mateus.conceicao@m8sistemas.com>
2023-10-25 14:36:40 -0300

    feat: create repositories

---
+++ tests/repositories/in-memory-challenges-repository.ts
@@ -0,0 +1,16 @@
+import { ChallengesRepository } from "../../src/application/repositories/ChallengesRepository";
+import { Challenge } from "../../src/domain/entities/challenge";
+
+export class InMemoryChallengesRepository implements ChallengesRepository {
+  public challenges: Challenge[] = [];
+
+  async findById(id: string): Promise<Challenge | null> {
+    const challenge = this.challenges.find((challenge) => challenge.id === id);
+
+    if (!challenge) {
+      return null;
+    }
+
+    return challenge;
+  }
+}
---
+++ tests/repositories/in-memory-students-repository.ts
@@ -0,0 +1,16 @@
+import { StudentsRepository } from "../../src/application/repositories/StudentsRepository";
+import { Student } from "../../src/domain/entities/student";
+
+export class InMemoryStudentsRepository implements StudentsRepository {
+  public students: Student[] = [];
+
+  async findById(id: string): Promise<Student | null> {
+    const student = this.students.find((student) => student.id === id);
+
+    if (!student) {
+      return null;
+    }
+
+    return student;
+  }
+}
